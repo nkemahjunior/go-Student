22a59beade1386b0e4daba7b69a6325858d5b922
nkemahjunior <nkemahjunior679205967@gmail.com>
2023-12-10 14:58:13 +0100

    fixed one useless error

--- src/BACKEND/StudentDetails/getStudentInfo.ts
+++ src/BACKEND/StudentDetails/getStudentInfo.ts
@@ -1,9 +1,20 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { supabaseServer } from "../supabaseServer";
+import { cookies } from "next/headers";
 
 
 export async function getStudentInfo(){
 try {
-    const supabase = supabaseServer()
+
+    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+    const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+    const cookieStore = cookies();
+    const supabase = createServerComponentClient(
+        { cookies: () => cookieStore },
+        { supabaseUrl, supabaseKey }
+    );
+    // const supabase = supabaseServer()
 
 
     const { data: { user },error:getuserError } = await supabase.auth.getUser()
@@ -14,7 +25,7 @@ try {
         let { data: profile, error } = await supabase
         .from('Profile')
         .select('*')
-        .eq('id',user?.id)
+        .eq('id',user?.id )
     
         if(error) throw new Error(error.message)
     
