a833c7e375e765f736e981fabd6084d9e81c126f
nkemahJunior <77687679+nkemahjunior@users.noreply.github.com>
2025-04-12 21:40:14 +0100

    update supabase url

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
           {
             protocol: "https",
-            hostname: "mlxrawglbrshmqmdkoch.supabase.co",
+            hostname: "tbxarkrrkxrmrsziuhri.supabase.co",
           },
         ],
       },
